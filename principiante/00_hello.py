f7698a23ea8e513863b9f857b4f83e28c70548f5
maria <marcavmer@alu.edu.gva.es>
2024-03-22 09:41:26 +0100

    hello.py

---
+++ principiante/00_hello.py
@@ -0,0 +1,18 @@
+# Esto es un comentario: Hola mundo
+print('Hola Python')
+print("Hola Mundo")
+
+
+"""
+Este es un comentario en 
+varias lineas
+Hola Mundo
+"""
+
+'''
+Este tambien es un 
+comentario
+'''
+#Consultar el tipo de dato
+print(type("Hola Python")) #Tipo 'str'
+
