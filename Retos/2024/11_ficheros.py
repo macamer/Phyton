a1ff1391940bea60fe8a10baad39f08205b7ba49
maria <marcavmer@alu.edu.gva.es>
2024-03-28 02:09:08 +0100

    finished reto 11

--- Retos/2024/11_ficheros.py
+++ Retos/2024/11_ficheros.py
@@ -21,16 +21,20 @@
  * - También debe poseer opciones para calcular la venta total y por producto.
  * - La opción salir borra el .txt.
  '''
+import os 
+
 file_name = "Retos/2024/macamer.txt"
 
 
 while True:
-    print("1.Añadir producto")
-    print("2.Consultar producto")
-    print("3.Actualizar producto")
-    print("4.Borrar producto")
-    print("5.Mostrar producto")
-    print("6.Añadir producto")
+    print("1. Añadir producto")
+    print("2. Consultar producto")
+    print("3. Actualizar producto")
+    print("4. Borrar producto")
+    print("5. Mostrar productos")
+    print("6. Calcular venta total")
+    print("7. Calcular venta por producto")
+    print("8. Salir")
     
     option = input("Selecciona una opción: ")
 
@@ -47,4 +51,66 @@ while True:
                 if line.split(", ")[0] == name:
                     print(line)
                     break
+    elif option == "3":
+        name = input("Nombre :")
+        quantity = input("Cantidad: ")
+        price = input("Precio: ")
+        with open(file_name, "r") as file:
+            lines = file.readlines()
+        with open (file_name, "w") as file:
+            for line in lines:
+                if line.split(", ")[0] == name:
+                    file.write(f"{name}, {quantity}, {price}\n")
+                else:
+                    file.write(line)
+    elif option == "4":
+        name = input("Name: ")
+        with open(file_name, "r") as file:
+            lines = file.readlines()
+        with open(file_name, "w") as file:
+            for line in lines:
+                if line.split(", ")[0] != name:
+                    file.write(line)
+    elif option == "5":
+        with open (file_name, "r") as file:
+            print(file.read())
+    elif option == "6":
+        total = 0
+        with open(file_name, "r") as file:
+            for line in file.readlines():
+                components = line.split(", ")
+                quantity = int(components[1])
+                price = float(components[2])
+                total += quantity * price
+        print(total)
+    elif option == "7":
+        name = input("Nombre: ")
+        total = 0
+        with open(file_name, "r") as file:
+            for line in file.readlines():
+                components = line.split(", ")
+                if components[0] == name:
+                    quantity = int(components[1])
+                    price = float(components[2])
+                    total += quantity * price
+                    break
+        print(total)
+    elif option == "7":
+        name = input("Nombre: ")
+        total = 0
+        with open(file_name, "r") as file:
+            for line in file.readlines():
+                components = line.split(", ")
+                if components[0] == name:
+                    quantity = int(components[1])
+                    price = float(components[2])
+                    total += quantity * price
+                    break
+        print(total)
+    elif option == "8":
+        os.remove(file_name)
+        break
+    else:
+        print("Selecciona una de las opciones disponibles.")
+                
                     
